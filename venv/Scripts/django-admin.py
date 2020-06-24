391fe319ce80c5d4bbdd7d58f6853914e33c40cb
Your Name <muhammad.umer2@mail.dcu.ie>
2020-06-24 13:16:59 +0100

    login page changes

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\umar\desktop\alt-intra\sanitizer-dispenser-system\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
